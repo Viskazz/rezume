b556fe872402ac8ee9be6ba1370c8faeacf562b9
tank <exppro1@gmail.com>
2020-06-20 20:50:33 +0300

    Updates

---
+++ precache-manifest.7cbc3dc848564b0677b7cd84ca46b955.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa564329d03a9f9289aa4d4a3093611",
+    "url": "/rezume/index.html"
+  },
+  {
+    "revision": "2cd44e106112aea64612",
+    "url": "/rezume/static/css/main.3604e659.chunk.css"
+  },
+  {
+    "revision": "32252c9b927cd79e176c",
+    "url": "/rezume/static/js/2.834f5ee5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rezume/static/js/2.834f5ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cd44e106112aea64612",
+    "url": "/rezume/static/js/main.b1b17d1d.chunk.js"
+  },
+  {
+    "revision": "2d4e91542fe73b84302e",
+    "url": "/rezume/static/js/runtime-main.39c4fbbd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rezume/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App name="Пронин Виктор" job="Web-разработчик" phone=" +7(977)832-73-68" email=" exppro1@gmail.com" />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
