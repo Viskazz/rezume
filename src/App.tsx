154636adf5a187cc59a868fc57cc3085e55bdb65
tank <exppro1@gmail.com>
2020-06-20 19:57:32 +0300

    comment useFetch, dont want api calls for this simple example-app

--- src/App.tsx
+++ src/App.tsx
@@ -32,9 +32,9 @@ function App(props: IAppProps) {
   })()
 
 
-  const data = useFetch('https://hn.algolia.com/api/v1/search?query=redux', {
-    method: 'GET'
-  })
+  // const data = useFetch('https://hn.algolia.com/api/v1/search?query=redux', {
+  //   method: 'GET'
+  // })
 
   return (
     <Fragment>
@@ -53,8 +53,6 @@ function App(props: IAppProps) {
             </nav>
           </div>
         </header>
-
-        {data.error && <div>Ошибка выполнения запроса ...</div>}
 
             <div className="App-main" >
               <JobName />
